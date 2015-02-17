3ffbbf1394d5c660752a4957ba0d08e5b6b8edab
Mario Mello <mario@dindigital.com>
2015-02-17 21:09:42 -0200

    Criação de teste inicial

---
+++ tests/CDC/Exemplos/ConversorDeNumeroRomanoTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace CDC\Exemplos;
+
+require "./vendor/autoload.php";
+
+use CDC\Exemplos\ConversorDeNumeroRomano;
+use PHPUnit_Framework_TestCase as PHPUnit;
+
+class ConversorDeNumeroRomanoTest extends PHPUnit
+{
+    public function testDeveEntenderOSimboloI()
+    {
+        $romano = new ConversorDeNumeroRomano;
+        $numero = $romano->converte('I');
+        $this->assertEquals(1, $numero);
+    }
+}
