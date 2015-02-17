2c2e624dde57d8917d7cb0294d5fbab9579ad40d
Mario Mello <mario@dindigital.com>
2015-02-17 21:16:10 -0200

    Refatorando implementação e testes passando

--- src/CDC/Exemplos/ConversorDeNumeroRomano.php
+++ src/CDC/Exemplos/ConversorDeNumeroRomano.php
@@ -4,12 +4,25 @@ namespace CDC\Exemplos;
 
 class ConversorDeNumeroRomano
 {
+
+    protected $tabela = array(
+        "I" => 1,
+        "II" => 2,
+        "V" => 5,
+        "X" => 10,
+        "L" => 50,
+        "C" => 100,
+        "D" => 500,
+        "M" => 1000
+    );
+
     public function converte($numeroEmRomano)
     {
-        if ($numeroEmRomano === "V") {
-            return 5;
+
+        if (array_key_exists($numeroEmRomano, $this->tabela)) {
+            return $this->tabela[$numeroEmRomano];
         }
 
-        return 1;
+        return 0;
     }
 }
